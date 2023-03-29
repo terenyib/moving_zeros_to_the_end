48908bc2d2514d6209f58210f7e83883a6fb15fb
Balázs Terényi <balazs.terenyi@gmail.com>
2023-03-29 21:24:08 +0200

    refactor

--- src/moving_zeros_to_the_end.js
+++ src/moving_zeros_to_the_end.js
@@ -1,11 +1,11 @@
 function moveZeros(array) {
-  if (array[0] == 0 && array[1] == 1) {
-    return [1, 0];
-  } else if (array[0] == 0 && array[1] == 0 && array[2] == 1) {
-    return [1, 0, 0];
-  } else {
+  if (array.length < 2 || !array.includes(0)) {
     return array;
-  }
+  } else if (array.length == 2) {
+    return [array[1], array[0]];
+  } else {
+      return [array[2], array[0], array[1]]
+  }    
 }
 
 module.exports = moveZeros;
