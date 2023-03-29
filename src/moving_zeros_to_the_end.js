25e8486d7321a0db9a87bb7780f43064929cfe74
Balázs Terényi <balazs.terenyi@gmail.com>
2023-03-29 22:07:59 +0200

    UAT 3.3 green

--- src/moving_zeros_to_the_end.js
+++ src/moving_zeros_to_the_end.js
@@ -4,11 +4,17 @@ function moveZeros(array) {
   } else if (array.length == 2) {
     return [array[1], array[0]];
   } else {
-    if (array[2] != 0) {
-      return [array[2], array[0], array[1]];
-    } else {
-      return [array[1], array[0], array[2]]
-    }      
+    let countOfZeroes = 0;
+    for(let i = array.length - 1; i >= 0; i--) {
+      if (array[i] == 0) {
+        countOfZeroes++;
+        array.splice(i, 1);        
+      }
+    }
+    for(let j = 0; j < countOfZeroes; j++) {
+      array.push(0);
+    }
+    return array;   
   }    
 }
 
