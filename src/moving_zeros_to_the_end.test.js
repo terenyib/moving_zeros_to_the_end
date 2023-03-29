bf6eac7d8de739008a1e45c9519d2822c2559ca4
Balázs Terényi <balazs.terenyi@gmail.com>
2023-03-29 20:36:25 +0200

    UAT 1.2 red

--- src/moving_zeros_to_the_end.test.js
+++ src/moving_zeros_to_the_end.test.js
@@ -4,4 +4,7 @@ describe('US-1', () => {
   it('Given the array "[0]" When I move zeros Then the result is "[0]"', () => {
     expect(moveZeros([0])).toStrictEqual([0]);
   });
+  it('Given the array "[1]" When I move zeros Then the result is "[1]"', () => {
+    expect(moveZeros([1])).toStrictEqual([1]);
+  });
 });
